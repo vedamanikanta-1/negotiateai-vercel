7e654a8c9327726f8eb4fdec6ade822635d6c187
vedamanikanta-1 <vedamanikantagannamani@gmail.com>
2026-06-06 22:56:17 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -41,8 +41,8 @@ module.exports = async function handler(req, res) {
         intent: "CAPTURE",
         purchase_units: [{
           amount: {
-            currency_code: "INR",
-            value: "199"
+            currency_code: "USD",
+            value: "2.40"
           },
           description: "NegotiateAI Full Report"
         }]
